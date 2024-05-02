c98fd00d2b769994367eabd54339834595762f48
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-05-02 22:20:35 +0530

    Update classroomRoutes.js

--- classroomRoutes.js
+++ classroomRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Classroom = require('../models/Classroom');
+const Classroom = require('./Classroom');
 const axios = require ('axios');
 
 // Create a classroom
